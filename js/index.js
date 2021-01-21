9518daf5cecad55cbfda91ab4471eceb4ba9aa46
Konstantin Pomazan <kospomazan@gmail.com>
2021-01-21 22:06:41 +0200

    got data from database

--- js/index.js
+++ js/index.js
@@ -1,9 +1,31 @@
+class FetchData {
+  async getResource(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  }
+
+  getPost() {
+    return this.getResource('db/dataBase.json');
+  }
+}
+
 class Twitter {
   constructor({ listItem }) {
-    this.tweets = new Posts({});
+    const fetchData = new FetchData();
+    this.tweets = new Posts();
     this.elements = {
       listItem: document.querySelector(listItem),
     };
+
+    fetchData.getPost()
+      .then(data => {
+        data.forEach(item => {
+          this.tweets.addPost(item);
+        }, twitter);
+      });
   }
 
   renderPosts() {}
@@ -18,17 +40,19 @@ class Twitter {
 }
 
 class Posts {
-  constructor(params) {
-    const { posts = [] } = params;
+  constructor({ posts = [] } = {}) {
     this.posts = posts;
   }
 
-  addPost(tweet) {
-    this.posts.push(new Post(tweet));
+  addPost(tweets) {
+    this.posts.push(tweets);
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    let updatedPosts = this.posts.filter(post => {
+      return post.id !== id;
+    });
+    this.posts = updatedPosts;
   }
 
   likePost() {
@@ -37,12 +61,11 @@ class Posts {
 }
 
 class Post {
-  constructor(params) {
-    const { id, userName, nickname, date, text, likes } = params;
-    this.id = id;
+  constructor({ id, userName, nickname, postDate, text, likes = 0 }) {
+    this.id = id || this.generateId();
     this.userName = userName;
     this.nickname = nickname;
-    this.date = date;
+    this.postDate = postDate ? new Date(postDate) : new Date();
     this.text = text;
     this.likes = likes;
     this.liked = false;
@@ -55,18 +78,24 @@ class Post {
     }
     this.likes--;
   }
+
+  generateId() {
+    return Math.random().toString(32).substring(2, 9) + (+new Date()).toString(32);
+  }
+
+  getDate() {
+    const options = {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+      hour: '2-digit',
+      minutes: '2-digit',
+    };
+
+    return this.postDate.toLocaleString('en-US', options);
+  }
 }
 
 const twitter = new Twitter({
   listItem: '.tweet-list'
-});
-
-twitter.tweets.addPost({
-  id: 'id',
-  userName: 'userName',
-  nickname: 'nickname',
-  date: 'date',
-  text: 'text',
-  likes: 'likes',
-  liked: 'false'
 });
